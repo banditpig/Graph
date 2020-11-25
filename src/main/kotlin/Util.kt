9092e24652cc0b35c6f7eea00ab27dd1ca1805a8
mike_k_houghton <mike_k_houghton@yahoo.co.uk>
2020-11-25 13:33:51 +0000

    Some refactor into fold(...) for outputDot function.

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -89,45 +89,55 @@ fun graphFromString(str: String):Graph<String>{
  */
 fun <T> outputDot(g: Graph<T>, path: Collection<Node<T>> = emptyList(), fName: String = "graph.dot") {
 
-
-
     val gName = "G"
     var doneEdges = mutableSetOf<String>()
     var dot = StringBuilder("graph $gName {").append("\n")
-    for(n in g.nodes()) {
-        val id = n.id.toString()
-        dot.append(id)
-        if (path.contains(n)) {
-            dot.append(" [color=red]").append("\n")
-        } else {
-            dot.append("\n")
-        }
-    }
-    //and loop again - this time adding edges.
-    for(n in g.nodes()){
-        val id = n.id.toString()
-          for (c in g.edgesFor(n)){
-            val edgeStr = id + " -- " + c.node.id.toString()
-            val edgeStrRev = c.node.id.toString() + " -- " + id
-            if (!doneEdges.contains(edgeStr) && !doneEdges.contains(edgeStrRev)){
-                dot.append(edgeStr)
-                val label = c.weightToParent.toString()
-                if (path.contains(c.node) && path.contains(n)){
-                    dot.append(" [label = $label, color=red ];")
-                }else {
-                    dot.append(" [label = $label];")
-                }
 
-                dot.append("\n")
-                doneEdges.add(edgeStr)
-                doneEdges.add(edgeStrRev)
+    fun buildNodes(sb: StringBuilder) {
+        fun nodeFunc(sb: StringBuilder, n: Node<T>):StringBuilder{
+            sb.append(n.id.toString())
+            if (path.contains(n)) {
+                sb.append(" [color=red]")
             }
 
-
+            return sb.append("\n")
         }
 
+        g.nodes().fold(initial = sb, operation = { acc, node -> nodeFunc(acc, node)})
+
     }
-    dot.append("}")
-    println(dot.toString())
+
+
+    fun buildEdges(sb: StringBuilder) {
+        g.nodes().forEach { n ->
+            val id = n.id.toString()
+            fun doEdge(str: StringBuilder,c: WeightedEdge<T>) : StringBuilder {
+                val edgeStr = id + " -- " + c.node.id.toString()
+                val edgeStrRev = c.node.id.toString() + " -- " + id
+                if (!doneEdges.contains(edgeStr) && !doneEdges.contains(edgeStrRev)) {
+                    str.append(edgeStr)
+                    val label = c.weightToParent.toString()
+                    if (path.contains(c.node) && path.contains(n)) {
+                        str.append(" [label = $label, color=red ];")
+                    } else {
+                        str.append(" [label = $label];")
+                    }
+
+                    str.append("\n")
+                    doneEdges.add(edgeStr)
+                    doneEdges.add(edgeStrRev)
+                }
+                return str
+            }
+
+            g.edgesFor(n).fold(sb, { acc, c -> doEdge(acc, c)})
+        }
+        sb.append("}")
+    }
+
+
+    buildNodes(dot)
+    buildEdges(dot)
+
     File(fName).writeText(dot.toString())
 }
